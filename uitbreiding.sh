29ae118cbf6f7edbbbaa443fee769a5e752415cb
Ilan Engels <ilan.engels@student.hogent.be>
2025-10-01 16:58:37 +0200

    uitbreiding

---
+++ uitbreiding.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -euo pipefail
+
+cat > app/Dockerfile << _EOF_
+FROM node:lts-alpine
+RUN apk add --no-cache python3 g++ make
+WORKDIR /app
+COPY . .
+RUN yarn install --production
+CMD ["node", "/app/src/index.js"]
+_EOF_
+
+cd app || exit
+docker build -t getting-started .
+docker ps -a 
